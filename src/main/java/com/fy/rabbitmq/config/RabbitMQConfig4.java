3a50de21ca8a8b07f2df57390719337d628500e7
fly_qv <fly_qv@outlook.com>
2022-07-03 18:29:24 +0800

    交换器 - topic 精准路由匹配

--- src/main/java/com/fy/rabbitmq/config/RabbitMQConfig4.java
+++ src/main/java/com/fy/rabbitmq/config/RabbitMQConfig4.java
@@ -24,39 +24,39 @@ import org.springframework.context.annotation.Configuration;
  * 　　　　　┗┻┛　┗┻┛
  *
  * @Author: fly
- * @Description: 交换器 - direct
+ * @Description: 交换器 - topic
  */
 
-//@Configuration
-public class RabbitMQConfig3 {
+@Configuration
+public class RabbitMQConfig4 {
 
     /**
-     *  学习 交换器 - direct 精准路由匹配
+     *  学习 交换器 -  topic 模糊路由匹配
      *
      */
     //1.1 创建交换器 用哪个交换器就创建哪个
     @Bean
-    public DirectExchange create(){
-        return new DirectExchange("ex-d");
+    public TopicExchange create(){
+        return new TopicExchange("ex-t");
     }
     //1.2 创建2个队列
     @Bean
-    public Queue createD1(){  //队列1
-        return new Queue("q_direct_01");
+    public Queue createT1(){  //队列1
+        return new Queue("q_topic_01");
     }
     @Bean
-    public Queue createD2(){ //队列2
-        return new Queue("q_direct_02");
+    public Queue createT2(){ //队列2
+        return new Queue("q_topic_02");
     }
     //1.3 创建队列和交换器绑定
     @Bean
-    public Binding createB1(DirectExchange de){
+    public Binding createB1(TopicExchange te){
         //把队列绑定到交换器上
         //with("log1")：路由关键字
-        return BindingBuilder.bind(createD1()).to(de).with("log");//绑定队列1
+        return BindingBuilder.bind(createT1()).to(te).with("user.*");//绑定队列1
     }
     @Bean
-    public Binding createB2(DirectExchange de){
-        return BindingBuilder.bind(createD2()).to(de).with("error");//绑定队列2
+    public Binding createB2(TopicExchange te){
+        return BindingBuilder.bind(createT2()).to(te).with("order.#");//绑定队列2
     }
 }
